7c98a144736bc6e65d81c1e4fcb341a7bca9c894
srahuliitb <srahul.iitb@gmail.com>
2023-12-06 21:43:08 +0000

    created NamedEntityClient that runs a spaCy nlp model to obtain named entities as a dictionary

---
+++ flask-ner/test/test_doubles.py
@@ -0,0 +1,30 @@
+class NerModelTestDouble:
+	"""
+	Test double for spaCy NLP model
+	"""
+	def __init__(self, model):
+		self.model = model
+
+	def returns_doc_ents(self, ents):
+		self.ents = ents
+
+	def __call__(self, sent):
+		return DocTestDouble(sent, self.ents)
+	
+class DocTestDouble:
+	"""
+	Test double for spaCy Doc
+	"""
+	def __init__(self, sent, ents):
+		self.ents = [SpanTestDouble(ent['text'], ent['label_']) for ent in ents]
+
+	# Optional patch_method
+	def patch_mode(self, attr, return_value):
+		def patched(): return return_value
+		setattr(self, attr, patched)
+		return self
+
+class SpanTestDouble:
+	def __init__(self, text, label):
+		self.text = text
+		self.label_ = label
